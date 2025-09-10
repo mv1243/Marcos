af96a7e2708dd3baea456d17b7782b19647d94ce
mv1243 <107856497+mv1243@users.noreply.github.com>
2025-09-10 15:33:18 -0300

    Xadrez 0.1

---
+++ Xadrez/Xadrez/Entity/Rei.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xadrez.Interface;
+
+namespace Xadrez.Entity
+{
+    public class Rei : IPeca
+    {
+        public string Cor { get; set; }
+
+        public string Tipo { get; set; }
+
+        public Rei(int cor)
+        {
+            if (cor == 1) Cor = "Branco";
+            if (cor == 2) Cor = "Preto";
+            Tipo = "Peça";
+        }
+
+        public void MoverPeca(IPeca rei)
+        {
+            static bool ValidarMovimento(int y1, int x1, int y2, int x2)
+            {
+                int[] localatual = { y1, x1 };
+
+                int[] localfuturo = { y2, x2 };
+
+                if (y1 - y2 <= 1 && y1 - y2 >= -1 && x1 <= 1 && x1 >= -1)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Xadrez/Xadrez/Interface/Peca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.Interface
+{
+    public interface IPeca
+    {
+        public string Cor { get; set; }
+
+        public string Tipo { get; set; }
+        public abstract void MoverPeca(IPeca entity);
+    }
+}
