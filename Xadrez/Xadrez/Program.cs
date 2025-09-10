d40f6b9cf9f276399a9cb65ccc11bf88fd469da1
mv1243 <107856497+mv1243@users.noreply.github.com>
2025-09-10 20:46:41 -0300

    Delete Xadrez directory

--- Xadrez/Xadrez/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Text;
-using Xadrez;
-
-Console.OutputEncoding = Encoding.UTF8;
-Tabuleiro tabuleiro = new Tabuleiro();
-tabuleiro.Implimentar_Tabuleiro();
